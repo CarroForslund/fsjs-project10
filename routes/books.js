b526480c65e4874ad458a205dca8b4a5f529c5c9
Caroline Forslund <cf222ev@student.lnu.se>
2020-04-08 17:53:13 +0100

    view updates

--- routes/books.js
+++ routes/books.js
@@ -15,11 +15,11 @@ function asyncHandler(cb){
 
 // List All
 router.get('/', (req, res) => {
-    res.render('all_books', {title: 'All Books'});
+    res.render('all-books', {title: 'All Books'});
 });
 // Create New Book Form
 router.get('/new', (req, res) => {
-    res.render('new_book', { book: {}, title: 'New Book'});
+    res.render('new-book', { book: {}, title: 'New Book'});
 });
 // Create New Book
 router.post('/', asyncHandler(async (req, res) => {
@@ -29,15 +29,7 @@ router.post('/', asyncHandler(async (req, res) => {
 }));
 //Get individual Book
 router.get('/:id', asyncHandler(async (req, res) => {
-    res.render('book_detail'), {book: {}, title: 'Book Title'};
+    res.render('update-book'), {book: {}, title: 'Book Title'};
 }));
-// List Overdue
-router.get('/books?filter=overdue', (req, res) => {
-    res.render('overdue_books'), {title: 'Overdue Books'};
-});
-// List Checked Out
-router.get('/books?filter=checked_out', (req, res) => {
-    res.render('checked_books', {title: 'Checked Out Books'});
-});
 
 module.exports = router;
