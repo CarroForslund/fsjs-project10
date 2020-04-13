2d8699a6fbe36b76a0ca31bd6a0a86790a9cb883
Caroline Forslund <cf222ev@student.lnu.se>
2020-04-13 13:44:39 +0100

    error handling added

--- app.js
+++ app.js
@@ -38,7 +38,12 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error', err);
+  if(err.status === 404){
+    res.render('page-not-found');
+  } else {
+    res.render('error', err);
+  }
+  
 });
 
 module.exports = app;
--- routes/books.js
+++ routes/books.js
@@ -9,7 +9,6 @@ function asyncHandler(cb){
         try{
             await cb(req, res, next)
         } catch(error) {
-            // res.render('error', {error: error})
             res.status(500).send(error);
         }
     }
@@ -20,28 +19,60 @@ router.get('/', asyncHandler(async (req, res) => {
     const books = await Book.findAll();
     res.render('index', { books, title: 'Books' });
 }));
+
 // Create New Book Form
 router.get('/new', (req, res) => {
     res.render('new-book', { book: {}, title: 'New Book' });
 });
+
 // Create New Book
 router.post('/new', asyncHandler(async (req, res) => {
-    const book = await Book.create(req.body);
-    res.redirect('/books/' + book.id);
+    let book;
+    try {
+        book = await Book.create(req.body);
+        res.redirect('/books/' + book.id);
+    } catch (error){
+        console.log('error');
+        if(error.name === 'SequelizeValidationError') {
+            book = await Book.build(req.body);
+            res.render('new-book', { book, errors: error.errors, title: book.title })
+        } else {
+            throw error; // error caught in the asyncHandler's catch block
+        } 
+    };
 }));
+
 //Get individual Book
 router.get('/:id', asyncHandler(async (req, res) => {
     const book = await Book.findByPk(req.params.id);
-    res.render('update-book', { book, title: book.title });
+    if (book){
+        res.render('update-book', { book, title: book.title });
+    } else {
+        res.sendStatus(404);
+        // res.render('error');
+    }
 }));
+
 //Update individual Book
 router.post('/:id', asyncHandler(async (req, res) => {
-    const book = await Book.findByPk(req.params.id);
-    await book.update(req.body);
-    res.redirect('/books/' + book.id);
+    let book;
+    try {
+        book = await Book.findByPk(req.params.id);
+        await book.update(req.body);
+        res.redirect('/books/' + book.id);
+    } catch (error){
+        if(error.name === 'SequelizeValidationError') {
+            book = await Book.build(req.body);
+            res.render('update-book', { book, errors: error.errors, title: book.title })
+        } else {
+            throw error; // error caught in the asyncHandler's catch block
+        }
+    };
 }));
-router.get('/:id/delete', asyncHandler(async (req, res) => {
-    const book = await Book.findByPk(req.params.id);
+
+// Delete book
+router.post('/:id/delete', asyncHandler(async (req, res) => {
+    book = await Book.findByPk(req.params.id);
     await book.destroy();
     res.redirect('/');
 }));
